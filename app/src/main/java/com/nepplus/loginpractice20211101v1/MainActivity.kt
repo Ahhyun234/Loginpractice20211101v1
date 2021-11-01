eb0d1aead19d747fd3f2ea44974946023f90238a
이아현 <suk03029@gmail.com>
2021-11-01 18:00:42 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/loginpractice20211101v1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.loginpractice20211101v1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
